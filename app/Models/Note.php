a7f3fe37249789c4cd0591b3f7204c11efea1b49
théoo <90846584+theokbokki@users.noreply.github.com>
2026-02-05 12:10:18 +0100

    Create notes table

---
+++ app/Models/Note.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
+class Note extends Model
+{
+    use SoftDeletes;
+    use HasSlug;
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('title')
+            ->saveSlugsTo('slug');
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+}
